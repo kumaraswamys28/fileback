dea6a5930df3d0eeb041e76c44e136e35f760bae
Manojkumbi <manojkumbi2803@gmail.com>
2025-09-14 09:00:32 +0530

    ignore

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const io = new Server(server, {
 
 socketHandler(io); 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
