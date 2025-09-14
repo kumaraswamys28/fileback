bf9a5ae73cc4b17d27771249826b6b6ad9d66b9d
Manojkumbi <manojkumbi2803@gmail.com>
2025-09-14 09:58:40 +0530

    backend done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 // routes/index.js
 import express from 'express';
+import { child } from 'winston';
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  child.process('ls -la', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
   res.send('Hello from API');
 });
+
 
 export default router;
