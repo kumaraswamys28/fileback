18deda9d612aa716fe98d3ee414609bac1e2ba90
Manojkumbi <manojkumbi2803@gmail.com>
2025-09-14 10:05:14 +0530

    this is final

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,7 @@ import { child } from 'winston';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  child.process('ls -la', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  });
+  
   res.send('Hello from API');
 });
 
